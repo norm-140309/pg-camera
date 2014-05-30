657774a36f1da76cce81f6e9aeb050d2a4f0f31c
Norm Glomski <nglomski@gmail.com>
2014-05-29 21:57:45 -0500

    testing

--- js/index.js
+++ js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -46,17 +28,45 @@ var app = {
 
         console.log('Received Event: ' + id);
     },
-
     takePicture: function() {
-      navigator.camera.getPicture( function( imageURI ) {
-        alert( imageURI );
-      },
-      function( message ) {
-        alert( message );
-      },
-      {
-        quality: 80,
-        destinationType: Camera.DestinationType.FILE_URI
-      });
+        navigator.camera.getPicture(function(imageURI) {
+            //alert(imageURI);
+            app.upload(imageURI);
+        },
+                function(message) {
+                    alert(message);
+                },
+                {
+                    quality: 80,
+                    destinationType: Camera.DestinationType.FILE_URI
+                })
+    },
+    upload: function(fileURL) {
+        var win = function(r) {
+            console.log("Code = " + r.responseCode);
+            console.log("Response = " + r.response);
+            console.log("Sent = " + r.bytesSent);
+        }
+
+        var fail = function(error) {
+            alert("An error has occurred: Code = " + error.code);
+            console.log("upload error source " + error.source);
+            console.log("upload error target " + error.target);
+        }
+
+        var options = new FileUploadOptions();
+        options.fileKey = "file";
+        options.fileName = fileURL.substr(fileURL.lastIndexOf('/') + 1);
+        options.mimeType = "text/plain";
+
+        var params = {};
+        params.value1 = "test";
+        params.value2 = "param";
+
+        options.params = params;
+
+        var ft = new FileTransfer();
+        ft.upload(fileURL, encodeURI("http://pg-camera.net/api/upload.php"), win, fail, options);
     }
+
 };
